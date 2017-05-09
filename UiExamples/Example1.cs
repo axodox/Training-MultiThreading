c2d422d92ab642a039a4e9e824dc2d168df2d506
Major Péter <axodox@outlook.com>
2017-05-09 09:24:01 +0200

    Added UiExamples project

---
+++ UiExamples/Example1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading;
+using System.Windows;
+
+namespace UiExamples
+{
+  /// <summary>
+  /// Using the dispatcher to update the UI
+  /// </summary>
+  class Example1
+  {
+    public ObservableCollection<string> Items { get; } = new ObservableCollection<string>();
+
+    public Example1()
+    {
+      new Thread(Worker).Start();
+    }
+
+    private void Worker()
+    {
+      Thread.CurrentThread.IsBackground = true;
+      var number = 0;
+      while (true)
+      {
+        Thread.Sleep(100);
+        //Items.Add($"Item {number++}"); -> runtime error
+        Application.Current.Dispatcher.Invoke(() => Items.Add($"Item {number++}"));
+      }
+    }
+  }
+}
